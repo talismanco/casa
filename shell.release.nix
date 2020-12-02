623a49c9808b15687c300eeb35ee113d00021fed
Sam Craig <contact@samcraig.io>
2020-12-02 18:59:23 -0500

    ci: update semantic-release pipeline

---
+++ shell.release.nix
@@ -0,0 +1,19 @@
+{ sources ? import ./nix {} }:
+let
+  inherit (sources)
+    toyboxpkgs
+    nixpkgs
+  ;
+in
+nixpkgs.mkShell rec {
+  name = "release.casa";
+  env = nixpkgs.buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    # <toyboxpkgs>
+    toyboxpkgs.jq_1_6
+    toyboxpkgs.nodejs_12_18_3
+    toyboxpkgs.python_3_7_7
+    # <nixpkgs>
+    # ...
+  ];
+}
